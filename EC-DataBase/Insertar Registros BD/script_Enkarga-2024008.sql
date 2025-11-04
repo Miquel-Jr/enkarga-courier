c4cb7e607cfb08ac1d879fee6e64d1a762c2d025
Miguel Isique <milanca221@gmail.com>
2025-11-04 08:21:43 -0500

    Mejoras en el modulo de guias

--- EC-DataBase/Insertar Registros BD/script_Enkarga-2024008.sql
+++ EC-DataBase/Insertar Registros BD/script_Enkarga-2024008.sql
@@ -2874,7 +2874,7 @@ TRUNCATE TABLE `tipo_documento`;
 INSERT INTO `tipo_documento` (`tipo_documento`, `descripcion`, `id_estado`) VALUES
 (1, 'DNI', 1),
 (2, 'CE', 1),
-(3, 'PASAPORTE', 1);
+(3, 'PASAPORTE', 1),
 (4, 'RUC', 1);
 
 -- --------------------------------------------------------
